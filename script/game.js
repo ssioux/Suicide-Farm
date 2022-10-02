37e078c7e2899097d09ace61271a288f853d9e74
David Lazaro <dlazaro7@gmail.com>
2022-10-02 19:13:00 +0200

    Starting Game with background

--- script/game.js
+++ script/game.js
@@ -4,31 +4,43 @@ class Game {
   constructor() {
     //! Properties
     // Fondo
+    this.floor = new Image();
+    this.floor.src = "./images/floor.jpeg";
     // Human
     // Pig
     // Chicken
     // Rabbit
     // bonus pajar
   }
+
+  // ! METHODS
+
+  // addPig
+  // addChicken
+  // addRabbit
+
+  // chickenHumanWound = () => {}
+  // rabbitHumanWound = () => {}
+  // pigHumanWound = () => {}
+
+  // pigGoesOut = Lost
+  // drawFloor
+  drawFloor = () => {
+
+    ctx.drawImage(this.floor, 0, 0, canvas.width, canvas.height)
+  }
+  // gameOver
+
+  gameLoop = () => {
+    // * 1. Limpiar canvas
+    ctx.clearRect(0, 0, canvas.Width, canvas.height)
+
+    // * 2. Acciones y movimientos de los elementos
+
+    // * 3. Dibujado de los elementos
+    this.drawFloor()
+    // * 4. Control de la recursion
+
+    requestAnimationFrame(this.gameLoop);
+  };
 }
-
-
-// ! METHODS
-
-// addPig
-// addChicken
-// addRabbit
-
-
-
-
-// chickenHumanWound = () => {}
-// rabbitHumanWound = () => {}
-// pigHumanWound = () => {}
-
-// pigGoesOut = Lost
-// gameOver
-
-// gameLoop = () => {}
-
-
--- script/main.js
+++ script/main.js
@@ -1,8 +1,33 @@
-console.log("testing")
-
+console.log("testing");
 // ! GLOBAL VARIABLES
-        // Dom
+
+const canvas = document.querySelector("#my-canvas");
+const ctx = canvas.getContext("2d");
+//                    * DOM *
+const startScreen = document.querySelector("#startmenu-screen");
+const startBtn = document.querySelector("#start-btn");
+const gameOverScreen = document.querySelector("#gameover-screen");
+const reStartBtn = document.querySelector("#restart-btn");
+
+let gameClass;
+
 // ! STATE MANAGEMENT FUNCTIONS
-        // startGame
-        // reStartGame
+// startGame
+const startGame = () => {
+  console.log("iniciando canvas");
+  startScreen.style.display = "none";
+  canvas.style.display = "grid";
+  gameClass = new Game()
+  gameClass.gameLoop()
+  
+};
+
+// reStartGame
+
+const reStartGame = () => {};
+
 // ! ADD EVENT LISTENERS
+
+startBtn.addEventListener("click", startGame);
+
+reStartBtn.addEventListener("click", reStartGame);
