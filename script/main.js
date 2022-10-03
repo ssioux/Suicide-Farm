3c55aacfbe304a3ee5bc0f5b9fc173e44d204fdf
David Lazaro <dlazaro7@gmail.com>
2022-10-03 11:00:40 +0200

    Movement Human added

--- script/main.js
+++ script/main.js
@@ -5,7 +5,7 @@ const canvas = document.querySelector("#my-canvas");
 const ctx = canvas.getContext("2d");
 //                    * DOM *
 const startScreen = document.querySelector("#startmenu-screen");
-const startBtn = document.querySelector("#start-btn");
+const startBtn = document.querySelector(".start-btn");
 const gameOverScreen = document.querySelector("#gameover-screen");
 const reStartBtn = document.querySelector("#restart-btn");
 
@@ -17,9 +17,8 @@ const startGame = () => {
   console.log("iniciando canvas");
   startScreen.style.display = "none";
   canvas.style.display = "grid";
-  gameClass = new Game()
-  gameClass.gameLoop()
-  
+  gameClass = new Game();
+  gameClass.gameLoop();
 };
 
 // reStartGame
@@ -29,5 +28,17 @@ const reStartGame = () => {};
 // ! ADD EVENT LISTENERS
 
 startBtn.addEventListener("click", startGame);
-
 reStartBtn.addEventListener("click", reStartGame);
+
+window.addEventListener("keydown", (event) => {
+  if (event.code === "ArrowRight" && gameClass.humanClass.x < canvas.width - gameClass.humanClass.w) {
+    gameClass.humanClass.x = gameClass.humanClass.x + 15 
+    console.log(gameClass.humanClass.x)
+  } else if (event.code === "ArrowLeft"){
+    gameClass.humanClass.x = gameClass.humanClass.x -15
+  } else if (event.code === "ArrowUp"){
+    gameClass.humanClass.y = gameClass.humanClass.y -15
+  } else if (event.code === "ArrowDown"){
+    gameClass.humanClass.y = gameClass.humanClass.y +15
+  }
+})
